68c63315e346686b1359225af4342c33dea148ab
data-pup <kevinmartinucr@gmail.com>
2018-04-16 12:46:53 -0700

    Starting some of the work to check if a state is solved.

--- src/solver/mod.rs
+++ src/solver/mod.rs
@@ -1,5 +1,5 @@
-use {Queens, UncontestedSpaces};
 use board::Board;
+use {Queens, UncontestedSpaces};
 
 pub struct Solver {
     board: Board,
@@ -11,7 +11,19 @@ impl Solver {
         unimplemented!();
     }
 
-    fn check_is_solved(b: Board) -> bool {
+    fn check_is_solved(b: &Board) -> bool {
+        let q_positions = b.get_queen_positions();
+        if q_positions.len() < 8 {
+            return false;
+        } else if q_positions.len() > 8 {
+            panic!("More than 8 queens are on the board.")
+        }
+
+        for &curr_pos in q_positions.iter() {
+            let moves = b.get_queen_moves(curr_pos);
+            unimplemented!(); // Check if any of the positions contain a queen.
+        }
+
         unimplemented!();
     }
 }
