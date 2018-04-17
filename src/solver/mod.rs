d21ebf7f1fba593c01235c3a39a2e5481904fb2f
data-pup <kevinmartinucr@gmail.com>
2018-04-17 14:26:53 -0700

    Continuing work on the 'solver' struct, which will probably be renamed at some point. Added extra fields that should help when navigating solutions.

--- src/solver/mod.rs
+++ src/solver/mod.rs
@@ -1,21 +1,26 @@
 use board::Board;
 use std::collections::HashSet;
-use {PosCoords, Queens, Solutions};
+use {PosCoords, Queens, Solutions, UncontestedSpaces};
 
 /// This struct is used to find solutions to the problem, given a board state.
 pub struct Solver {
-    _board: Board,
-    _is_solved: bool,
+    is_solved: bool,
+    all_queens_safe: bool,
 }
 
 impl Solver {
     /// Create a new solver object.
-    fn _new(_board: Board) -> Solver {
-        unimplemented!();
+    pub fn _new(_board: &Board) -> Solver {
+        let is_solved = Solver::check_is_solved(_board);
+        let all_queens_safe = Solver::has_no_contested_queens(_board);
+        Solver {
+            is_solved,
+            all_queens_safe,
+        }
     }
 
     /// Check if the board is currently a solution to the 8 queens problem.
-    fn _check_is_solved(b: &Board) -> bool {
+    fn check_is_solved(b: &Board) -> bool {
         let q_positions = b.get_queen_positions();
         if q_positions.len() < 8 {
             return false;
@@ -27,8 +32,16 @@ impl Solver {
             let _moves = b.get_queen_moves(curr_pos);
             unimplemented!(); // Check if any of the positions contain a queen.
         }
-
         unimplemented!(); // Fixup.
+    }
+
+    fn has_no_contested_queens(b: &Board) -> bool {
+        let q_positions = b.get_queen_positions();
+        let uncontested = b.get_uncontested_spaces();
+        q_positions
+            .iter()
+            .map(|p| uncontested.contains(p))
+            .fold(true, |res, curr| res && curr)
     }
 }
 
@@ -43,9 +56,11 @@ impl Solutions for Solver {
 
 #[cfg(test)]
 mod solver_tests {
+    use super::Board;
+
     #[test]
     fn default_board_is_not_a_solution() {
-        unimplemented!();
+        unimplemented!()
     }
 
     #[test]
