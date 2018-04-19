8626814026f90584c66b69d417798eb2ac2c56a5
data-pup <kevinmartinucr@gmail.com>
2018-04-19 10:43:43 -0700

    Added some drafting of the board printing logic. Keeping this commented out for now, but this might come in handy later.

--- src/board/board_to_string.rs
+++ src/board/board_to_string.rs
@@ -1,8 +1,27 @@
 use super::Board;
 use std::string::ToString;
 
+static _BORDER: &'static str = " |ABCDEFG|";
+
 impl ToString for Board {
     fn to_string(&self) -> String {
         unimplemented!();
+    }
+}
+
+impl Board {
+    fn _get_row_strings(&self) -> Vec<String> {
+        // let temp = (0..self.height)
+        //     .map(|row| {
+        //         self.get_row_indices(row)
+        //         .into_iter()
+        //         .map(|i| self.squares[i])
+        //          // todo todo
+        //     })
+        unimplemented!();
+    }
+
+    fn _get_row_indices(&self, row: u32) -> Vec<usize> {
+        (0..self.width).map(|col| self.get_pos_index(row, col)).collect()
     }
 }
