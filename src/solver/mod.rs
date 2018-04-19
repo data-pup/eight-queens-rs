2d69b4e324a04b8c6bc184cfc5123794ee412e91
data-pup <kevinmartinucr@gmail.com>
2018-04-19 10:23:45 -0700

    Added benchmarks for the get_next_moves method, which seems to be where much of the delay is happening.

--- src/solver/mod.rs
+++ src/solver/mod.rs
@@ -48,10 +48,12 @@ impl Solutions for Solver {
 
 #[cfg(test)]
 mod get_next_moves_bench {
+    extern crate rand;
     extern crate test;
     use self::test::Bencher;
+    use rand::Rng;
     use super::Solver;
-    use Board;
+    use {Board, PosCoords};
 
     #[bench]
     fn time_get_next_moves_for_empty_board(bencher: &mut Bencher) {
@@ -60,5 +62,43 @@ mod get_next_moves_bench {
         bencher.iter(|| {
             let _ = s._get_next_moves();
         });
+    }
+
+    #[bench]
+    fn time_get_next_moves_for_board_with_two_queens(bencher: &mut Bencher) {
+        let b: Board = get_n_random_coords(2).into_iter().collect();
+        let s = Solver::_new(b);
+        bencher.iter(|| {
+            let _ = s._get_next_moves();
+        });
+    }
+
+    #[bench]
+    fn time_get_next_moves_for_board_with_four_queens(bencher: &mut Bencher) {
+        let b: Board = get_n_random_coords(4).into_iter().collect();
+        let s = Solver::_new(b);
+        bencher.iter(|| {
+            let _ = s._get_next_moves();
+        });
+    }
+
+    #[bench]
+    fn time_get_next_moves_for_board_with_seven_queens(bencher: &mut Bencher) {
+        let b: Board = get_n_random_coords(7).into_iter().collect();
+        let s = Solver::_new(b);
+        bencher.iter(|| {
+            let _ = s._get_next_moves();
+        });
+    }
+
+    fn get_n_random_coords(n: usize) -> Vec<PosCoords> {
+        let mut rng = rand::thread_rng();
+        let mut y_range = (0..8).collect::<Vec<u32>>();
+        let mut x_range = (0..8).collect::<Vec<u32>>();
+        rng.shuffle(&mut x_range);
+        rng.shuffle(&mut y_range);
+        let x_coords: Vec<u32> = x_range.into_iter().take(n).collect();
+        let y_coords: Vec<u32> = y_range.into_iter().take(n).collect();
+        (0..n).map(|i| (x_coords[i], y_coords[i])).collect()
     }
 }
